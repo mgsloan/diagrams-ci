35e5a918442a984fab64e87303a2ae5a17336b42
Brent Yorgey <byorgey@gmail.com>
2015-11-04 11:29:48 -0600

    Ubuntu packages to ensure are installed

--- build-universe.sh
+++ build-universe.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+UBUNTU_PKGS=(
+
+  'pkg-config'
+  'glib-2.0-dev'
+  'libcairo2-dev'
+  'libpango1.0-dev'
+  'libgtk2.0-dev'
+  'python-docutils'
+  )
+
+apt-get install -y "${UBUNTU_PKGS[@]}"
+
 REPOS=(
 
   # support libraries
