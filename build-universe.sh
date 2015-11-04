7a6d7cb944389a51d8b522c55e5af1e4ff8a7a4d
Brent Yorgey <byorgey@gmail.com>
2015-11-04 11:51:46 -0600

    move Ubuntu package installation into separate script

--- build-universe.sh
+++ build-universe.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-UBUNTU_PKGS=(
-
-  'pkg-config'
-  'glib-2.0-dev'
-  'libcairo2-dev'
-  'libpango1.0-dev'
-  'libgtk2.0-dev'
-  'python-docutils'
-  )
-
-apt-get install -y "${UBUNTU_PKGS[@]}"
 
 REPOS=(
 
---
+++ install-ubuntu-pkgs.sh
@@ -0,0 +1,11 @@
+UBUNTU_PKGS=(
+
+  'pkg-config'
+  'glib-2.0-dev'
+  'libcairo2-dev'
+  'libpango1.0-dev'
+  'libgtk2.0-dev'
+  'python-docutils'
+  )
+
+apt-get install -y "${UBUNTU_PKGS[@]}"
